c08d680d4ae5750cdb2945ae776462d8964f71d0
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-13 11:41:41 +0200

    Update 09_qiime2_calculate_and_explore_diversity_metrics_PE.sh

--- 00_scripts/09_qiime2_calculate_and_explore_diversity_metrics_PE.sh
+++ 00_scripts/09_qiime2_calculate_and_explore_diversity_metrics_PE.sh
@@ -27,7 +27,7 @@ TMPDIR=/scratch_vol0
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
+conda activate /scratch_vol0/fungi/envs/qiime2-amplicon-2024.10
 
 # Make the directory (mkdir) only if not existe already(-p)
 mkdir -p pcoa
