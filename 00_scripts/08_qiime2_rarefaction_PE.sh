2698d4752c0c4232d74cc52ca6005631bf3ee9cb
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-13 12:58:38 +0200

    Update 08_qiime2_rarefaction_PE.sh

--- 00_scripts/08_qiime2_rarefaction_PE.sh
+++ 00_scripts/08_qiime2_rarefaction_PE.sh
@@ -79,7 +79,7 @@ TMPDIR=/scratch_vol0
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
+conda activate /scratch_vol0/fungi/envs/qiime2-amplicon-2024.10
 
 # I'm doing this step in order to deal the no space left in cluster :
 export TMPDIR='/scratch_vol0/fungi'
@@ -141,7 +141,7 @@ TMPDIR=/scratch_vol0
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
+conda activate /scratch_vol0/fungi/envs/qiime2-amplicon-2024.10
 
 # I'm doing this step in order to deal the no space left in cluster :
 export TMPDIR='/scratch_vol0/fungi'
@@ -203,7 +203,7 @@ TMPDIR=/scratch_vol0
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
+conda activate /scratch_vol0/fungi/envs/qiime2-amplicon-2024.10
 
 # I'm doing this step in order to deal the no space left in cluster :
 export TMPDIR='/scratch_vol0/fungi'
