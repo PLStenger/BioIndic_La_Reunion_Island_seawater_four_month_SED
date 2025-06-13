e1318f0bf97fe98026b26e76fa475e88caba7d00
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2025-06-13 11:40:50 +0200

    Update 06_qiime2_denoise_PE_no_contamination.sh

--- 00_scripts/06_qiime2_denoise_PE_no_contamination.sh
+++ 00_scripts/06_qiime2_denoise_PE_no_contamination.sh
@@ -17,7 +17,7 @@ TMPDIR=/scratch_vol0
 cd $WORKING_DIRECTORY
 
 eval "$(conda shell.bash hook)"
-conda activate qiime2-2021.4
+conda activate /scratch_vol0/fungi/envs/qiime2-amplicon-2024.10
 
 # Définir le répertoire temporaire pour éviter le problème d’espace
 export TMPDIR='/scratch_vol0/fungi'
